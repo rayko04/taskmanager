0e7d8f1cc882e57b3c3f3163236d81da9cc10fbb
rayko04 <mrayyanadeel4@gmail.com>
2026-08-26 11:50:21 +0500

    refactored

--- main.go
+++ main.go
@@ -13,184 +13,218 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, taskmanager.")
 }
 
-//closure so handler can access repo
-func createTasksHandler(repo *repository.TaskRepository) http.HandlerFunc{
-
-	return func (w http.ResponseWriter, r *http.Request) {
-		
-		//trim ending "/"
-		pathstr := strings.TrimRight(r.URL.Path, "/")
+func trimAndSplit(str string) []string {
+		pathstr := strings.TrimRight(str, "/")
 		path := strings.Split(pathstr, "/")
 
+		return path
+}
+
+func endsInTasks(path []string) bool {
+	return len(path) == 2 && path[len(path)-1] == "tasks"
+}
+
+func endsInTaskId(path []string) bool {
+	return len(path) == 3 && path[len(path)-2] == "tasks"
+}
+
+//handles getall()
+func getTasksHandler(repo *repository.TaskRepository) http.HandlerFunc{
+
+	return func (w http.ResponseWriter, r *http.Request) {
+
+		tasks := repo.GetAll()
+		
+		data, err := json.Marshal(tasks)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	}
+}
+
+func getTaskHandler(repo *repository.TaskRepository) http.HandlerFunc{
+
+	return func (w http.ResponseWriter, r *http.Request) {
+
+		path := trimAndSplit(r.URL.Path)
+
+		id, error := strconv.Atoi(path[len(path)-1])
+		if error != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		
+		task, exists := repo.GetById(id)
+		if !exists {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+
+		data, err := json.Marshal(task)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	}
+}
+
+//handles POST
+func createTaskHandler(repo *repository.TaskRepository) http.HandlerFunc{
+
+	return func (w http.ResponseWriter, r *http.Request) {
+
+		//json to go
+		decoder := json.NewDecoder(r.Body)
+		req := &model.TaskRequest{}		//Decode requires a pointer
+		err := decoder.Decode(req)
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		task := model.Task {
+			Title: req.Title,
+			Description: req.Description,
+		}
+		task = repo.Create(task)
+
+		//send back response in json
+		data, err := json.Marshal(task)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		w.Write(data)
+	}
+}
+
+//handles PUT
+func updateTaskHandler(repo *repository.TaskRepository) http.HandlerFunc{
+
+	return func (w http.ResponseWriter, r *http.Request) {
+
+		path := trimAndSplit(r.URL.Path)
+
+		
+		id, error := strconv.Atoi(path[len(path)-1])
+		if error != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		req := &model.TaskUpdateRequest{}	//Decode requires a pointer
+		err := decoder.Decode(req)
+		if err != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		task := model.Task {
+			Title: req.Title,
+			Description: req.Description,
+			Completed: req.Completed,
+		}
+		updated := false
+
+		task, updated = repo.Update(id, task)
+		if !updated {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+
+		//send back response in json
+		data, err := json.Marshal(task)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(data)
+	}
+}
+
+func deleteTaskHandler(repo *repository.TaskRepository) http.HandlerFunc{
+
+	return func (w http.ResponseWriter, r *http.Request) {
+
+		path := trimAndSplit(r.URL.Path)
+
+		id, error := strconv.Atoi(path[len(path)-1])
+		if error != nil {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		deleted := repo.Delete(id)
+		if !deleted {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusNoContent)
+	}
+}
+
+func collectionDispatcher(repo *repository.TaskRepository) http.HandlerFunc {
+	
+	return func (w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
-
-			//GET
 		case http.MethodGet:
-			
-			//if ends in tasks
-			if len(path) == 2 && path[len(path)-1] == "tasks" {
-				tasks := repo.GetAll()
-				
-				data, err := json.Marshal(tasks)
-				if err != nil {
-					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-					return
-				}
-
-				w.Header().Set("Content-Type", "application/json")
-				w.Write(data)
-				
-			} else if len(path) == 3 && path[len(path)-2] == "tasks" {
-				//if ends in tasks/[smthin]
-
-				id, error := strconv.Atoi(path[len(path)-1])
-				if error != nil {
-					http.Error(w, "Bad Request", http.StatusBadRequest)
-					return
-				}
-				
-				task, exists := repo.GetById(id)
-				if !exists {
-					http.Error(w, "Not Found", http.StatusNotFound)
-					return
-				}
-
-				data, err := json.Marshal(task)
-				if err != nil {
-					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-					return
-				}
-
-				w.Header().Set("Content-Type", "application/json")
-				w.Write(data)
-			} else {
-				http.Error(w, "Not Found", http.StatusNotFound)
-				return
-			}
-
-			//POST
+			getTasksHandler(repo)(w, r)
 		case http.MethodPost:
-
-			if len(path) != 2 || path[len(path)-1] != "tasks" {
-				http.Error(w, "Bad Request", http.StatusBadRequest)
-				return
-			}
-			//json to go
-			decoder := json.NewDecoder(r.Body)
-			req := &model.TaskRequest{}		//Decode requires a pointer
-			err := decoder.Decode(req)
-			if err != nil {
-				http.Error(w, "Bad Request", http.StatusBadRequest)
-				return
-			}
-
-			task := model.Task {
-				Title: req.Title,
-				Description: req.Description,
-			}
-			task = repo.Create(task)
-
-			//send back response in json
-			data, err := json.Marshal(task)
-			if err != nil {
-				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated)
-			w.Write(data)
-
-		case http.MethodDelete: 
-
-			//if ends in tasks
-			if len(path) == 2 && path[len(path)-1] == "tasks" {
-				http.Error(w, "Bad Request", http.StatusBadRequest)
-				return
-			} else if len(path) == 3 && path[len(path)-2] == "tasks" {
-				//if ends in tasks/[smthin]
-
-				id, error := strconv.Atoi(path[len(path)-1])
-				if error != nil {
-					http.Error(w, "Bad Request", http.StatusBadRequest)
-					return
-				}
-
-				deleted := repo.Delete(id)
-				if !deleted {
-					http.Error(w, "Not Found", http.StatusNotFound)
-					return
-				}
-				w.WriteHeader(http.StatusNoContent)
-			} else {
-				http.Error(w, "Bad Request", http.StatusBadRequest)
-				return
-			}
-
-		case http.MethodPut: 
-
-				//if ends in tasks
-			if len(path) == 2 && path[len(path)-1] == "tasks" {
-				http.Error(w, "Bad Request", http.StatusBadRequest)
-				return
-			} else if len(path) == 3 && path[len(path)-2] == "tasks" {
-				//if ends in tasks/[smthin]
-
-				id, error := strconv.Atoi(path[len(path)-1])
-				if error != nil {
-					http.Error(w, "Bad Request", http.StatusBadRequest)
-					return
-				}
-
-				decoder := json.NewDecoder(r.Body)
-				req := &model.TaskUpdateRequest{}	//Decode requires a pointer
-				err := decoder.Decode(req)
-				if err != nil {
-					http.Error(w, "Bad Request", http.StatusBadRequest)
-					return
-				}
-
-				task := model.Task {
-					Title: req.Title,
-					Description: req.Description,
-					Completed: req.Completed,
-				}
-				updated := false
-
-				task, updated = repo.Update(id, task)
-				if !updated {
-					http.Error(w, "Not Found", http.StatusNotFound)
-					return
-				}
-
-				//send back response in json
-				data, err := json.Marshal(task)
-				if err != nil {
-					http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-					return
-				}
-
-				w.Header().Set("Content-Type", "application/json")
-				w.Write(data)
-			} else {
-				http.Error(w, "Bad Request", http.StatusBadRequest)
-				return
-			}
-
+			createTaskHandler(repo)(w, r)
 		default:
 			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
 		}
-	}	
+	}
+}
+
+func individualDispatcher(repo*repository.TaskRepository) http.HandlerFunc {
+
+	return func (w http.ResponseWriter, r *http.Request) {
+
+		path := trimAndSplit(r.URL.Path)
+		if endsInTasks(path) {
+			http.Redirect(w, r, "/tasks", http.StatusMovedPermanently)
+			return
+		} else if endsInTaskId(path) {
+			switch r.Method {
+			case http.MethodGet:
+				getTaskHandler(repo)(w, r)
+			case http.MethodDelete:
+				deleteTaskHandler(repo)(w, r)
+			case http.MethodPut:
+				updateTaskHandler(repo)(w, r)
+			default:
+				http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+				return
+			}
+		} else {
+			http.Error(w, "Not Found", http.StatusNotFound)
+			return
+		}
+	}
 }
 
 func main() {
 
 	repo := repository.NewTaskRepository()
-	tasksHandler := createTasksHandler(repo)	//dependency injection through closure
 
 	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/tasks", tasksHandler)
-	http.HandleFunc("/tasks/", tasksHandler)
+	http.HandleFunc("/tasks", collectionDispatcher(repo))
+	http.HandleFunc("/tasks/", individualDispatcher(repo))
 
 	port := ":8080"
  
